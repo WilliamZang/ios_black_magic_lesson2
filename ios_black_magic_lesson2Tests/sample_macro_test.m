f334b8c8894af2eaf8bfbf2f9002b2aa3850e88f
WilliamZang <chengwei.zang.1985@gmail.com>
2017-02-06 00:07:39 +0800

    # 宏语法的小例子

--- ios_black_magic_lesson2Tests/sample_macro_test.m
+++ ios_black_magic_lesson2Tests/sample_macro_test.m
@@ -21,5 +21,22 @@ describe(@"sample_macro_test", ^{
         NSLog(@"MY_MACRO is %d", MY_MACRO);
     });
     
+    it(@"can use # to transform a parameter to string", ^{
+#define STR(arg)    #arg
+        NSLog(@"%s", STR(no more qutoes));
+        
+#define OCSTR(arg)  @#arg
+        NSLog(OCSTR(Correct?));
+    });
+    
+    it(@"can use # with log", ^{
+#define TRUE_AND_LOG(condition)                              \
+    do {                                                     \
+        if (condition) {                                     \
+            NSLog(@"Your condition '"#condition"' is true"); \
+        }                                                    \
+    } while(0)
+        TRUE_AND_LOG(1 + 1 == 2);
+    });
 });
 SpecEnd
