67258ab25806e0f33c3e869ae8922e47610c5316
WilliamZang <chengwei.zang.1985@gmail.com>
2017-02-10 01:58:17 +0800

    可变参数宏的小例子1

---
+++ ios_black_magic_lesson2Tests/condition_macro_test.m
@@ -0,0 +1,44 @@
+//
+//  condition_macro3_test.m
+//  ios_black_magic_lesson2
+//
+//  Created by Chengwei Zang on 2017/2/9.
+//  Copyright © 2017年 Chengwei Zang. All rights reserved.
+//
+
+#import <Expecta/Expecta.h>
+#import <Specta/Specta.h>
+
+#define HAS_FLAG
+
+#ifdef HAS_FLAG
+#define VALUE 5
+#else
+#define VALUE 6
+#endif
+
+#if 15 > 3
+#define VALUE2 5
+#else
+#define VALUE2 1
+#endif
+
+#define ARG_AT(INDEX, ...) _ARG_AT##INDEX(__VA_ARGS__)
+
+#define _ARG_AT0(_0, ...) _0
+#define _ARG_AT1(_0, _1, ...) _1
+#define _ARG_AT2(_0, _1, _2, ...) _2
+#define _ARG_AT3(_0, _1, _2, _3, ...) _3
+#define _ARG_AT4(_0, _1, _2, _3, _4, ...) _4
+#define _ARG_AT5(_0, _1, _2, _3, _4, _5, ...) _5
+
+SpecBegin(condition_macro_test)
+
+describe(@"condition_macro_test", ^{
+    it(@"should find element at args", ^{
+        int value = ARG_AT(3, 100, 200, 300, 400);
+        expect(value).to.equal(400);
+    });
+});
+
+SpecEnd
